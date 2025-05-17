9d9a20338a9852d49505982f65f63eb85c3cc95b
Alshanex <discordpermacc02@gmail.com>
2025-05-17 22:41:00 +0200

    2 new traits added

---
+++ src/main/java/net/alshanex/arcane_hostility/traits/MagicRebound.java
@@ -0,0 +1,41 @@
+package net.alshanex.arcane_hostility.traits;
+
+import dev.xkmc.l2damagetracker.contents.attack.DamageData;
+import dev.xkmc.l2hostility.content.traits.base.MobTrait;
+import io.redspace.ironsspellbooks.entity.spells.AbstractMagicProjectile;
+import net.minecraft.ChatFormatting;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
+
+public class MagicRebound extends MobTrait {
+    public MagicRebound(ChatFormatting format) {
+        super(format);
+    }
+
+    @Override
+    public boolean onAttackedByOthers(int level, LivingEntity entity, DamageData.Attack event) {
+        if(event.getSource().getDirectEntity() instanceof AbstractMagicProjectile projectile){
+            LivingEntity target = event.getTarget();
+
+            Vec3 targetPos = target.position().add(0, target.getBbHeight() / 2, 0);
+            Vec3 projectilePos = projectile.position();
+
+            Vec3 direction = projectilePos.subtract(targetPos).normalize();
+
+            Vec3 currentMotion = projectile.getDeltaMovement();
+            float speed = (float) currentMotion.length();
+
+            float bounceFactor = 0.8f;
+
+            projectile.setDeltaMovement(direction.scale(speed * bounceFactor));
+
+            projectile.setPos(
+                    projectile.getX() + direction.x * 0.5,
+                    projectile.getY() + direction.y * 0.5,
+                    projectile.getZ() + direction.z * 0.5
+            );
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/net/alshanex/arcane_hostility/traits/MagicSchoolImmunity.java
@@ -0,0 +1,24 @@
+package net.alshanex.arcane_hostility.traits;
+
+import dev.xkmc.l2damagetracker.contents.attack.DamageData;
+import dev.xkmc.l2hostility.content.traits.legendary.LegendaryTrait;
+import io.redspace.ironsspellbooks.api.spells.SchoolType;
+import net.minecraft.ChatFormatting;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.entity.LivingEntity;
+
+public class MagicSchoolImmunity extends LegendaryTrait {
+    private SchoolType school;
+
+    public MagicSchoolImmunity(ChatFormatting format, SchoolType school) {
+        super(format);
+        this.school = school;
+    }
+
+    @Override
+    public boolean onAttackedByOthers(int level, LivingEntity entity, DamageData.Attack event) {
+        return !event.getSource().is(DamageTypeTags.BYPASSES_INVULNERABILITY) &&
+                !event.getSource().is(DamageTypeTags.BYPASSES_EFFECTS) &&
+                event.getSource().is(this.school.getDamageType());
+    }
+}

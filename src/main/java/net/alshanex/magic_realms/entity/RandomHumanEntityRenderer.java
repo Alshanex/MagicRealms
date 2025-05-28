a4580dda7f7e98375d326e47ba0904420c75702c
Alshanex <discordpermacc02@gmail.com>
2025-05-28 11:26:37 +0200

    Shield fixed

--- src/main/java/net/alshanex/magic_realms/entity/RandomHumanEntityRenderer.java
+++ src/main/java/net/alshanex/magic_realms/entity/RandomHumanEntityRenderer.java
@@ -22,55 +22,13 @@ public class RandomHumanEntityRenderer extends AbstractSpellCastingMobRenderer {
 
     protected ItemStack mainHandItem;
     protected ItemStack offhandItem;
+    protected ItemStack hiddenShield = ItemStack.EMPTY;
+    private boolean shieldWasHidden = false;
 
     public RandomHumanEntityRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new RandomHumanEntityModel());
 
-        addRenderLayer(new BlockAndItemGeoLayer<>(this) {
-            @Nullable
-            @Override
-            protected ItemStack getStackForBone(GeoBone bone, AbstractSpellCastingMob animatable) {
-                // Retrieve the items in the entity's hands for the relevant bone
-                return switch (bone.getName()) {
-                    case LEFT_HAND -> animatable.isLeftHanded() ?
-                            RandomHumanEntityRenderer.this.mainHandItem : RandomHumanEntityRenderer.this.offhandItem;
-                    case RIGHT_HAND -> animatable.isLeftHanded() ?
-                            RandomHumanEntityRenderer.this.offhandItem : RandomHumanEntityRenderer.this.mainHandItem;
-                    default -> null;
-                };
-            }
-
-            @Override
-            protected ItemDisplayContext getTransformTypeForStack(GeoBone bone, ItemStack stack, AbstractSpellCastingMob animatable) {
-                // Apply the camera transform for the given hand
-                return switch (bone.getName()) {
-                    case LEFT_HAND, RIGHT_HAND -> ItemDisplayContext.THIRD_PERSON_RIGHT_HAND;
-                    default -> ItemDisplayContext.NONE;
-                };
-            }
-
-            // Do some quick render modifications depending on what the item is
-            @Override
-            protected void renderStackForBone(PoseStack poseStack, GeoBone bone, ItemStack stack, AbstractSpellCastingMob animatable,
-                                              MultiBufferSource bufferSource, float partialTick, int packedLight, int packedOverlay) {
-                if (stack == RandomHumanEntityRenderer.this.mainHandItem) {
-                    poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
-
-                    if (stack.getItem() instanceof ShieldItem)
-                        poseStack.translate(0, 0.125, -0.25);
-                }
-                else if (stack == RandomHumanEntityRenderer.this.offhandItem) {
-                    poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
-
-                    if (stack.getItem() instanceof ShieldItem) {
-                        poseStack.translate(0, 0.125, 0.25);
-                        poseStack.mulPose(Axis.YP.rotationDegrees(180));
-                    }
-                }
-
-                super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
-            }
-        });
+        addRenderLayer(new CustomItemGeoLayer(this));
     }
 
     @Override
@@ -84,5 +42,94 @@ public class RandomHumanEntityRenderer extends AbstractSpellCastingMobRenderer {
 
         this.mainHandItem = animatable.getMainHandItem();
         this.offhandItem = animatable.getOffhandItem();
+
+        if (this.offhandItem.getItem() instanceof ShieldItem) {
+            this.hiddenShield = this.offhandItem.copy();
+            animatable.setItemSlot(net.minecraft.world.entity.EquipmentSlot.OFFHAND, ItemStack.EMPTY);
+            this.shieldWasHidden = true;
+        }
+    }
+
+    private class CustomItemGeoLayer extends BlockAndItemGeoLayer<AbstractSpellCastingMob> {
+
+        public CustomItemGeoLayer(RandomHumanEntityRenderer renderer) {
+            super(renderer);
+        }
+
+        @Nullable
+        @Override
+        protected ItemStack getStackForBone(GeoBone bone, AbstractSpellCastingMob animatable) {
+            return switch (bone.getName()) {
+                case LEFT_HAND -> {
+                    ItemStack leftItem = animatable.isLeftHanded() ?
+                            RandomHumanEntityRenderer.this.mainHandItem :
+                            RandomHumanEntityRenderer.this.offhandItem;
+
+                    if (shouldRenderCustom(leftItem)) {
+                        yield leftItem;
+                    }
+                    yield null;
+                }
+                case RIGHT_HAND -> {
+                    ItemStack rightItem = animatable.isLeftHanded() ?
+                            RandomHumanEntityRenderer.this.offhandItem :
+                            RandomHumanEntityRenderer.this.mainHandItem;
+
+                    if (shouldRenderCustom(rightItem)) {
+                        yield rightItem;
+                    }
+                    yield null;
+                }
+                default -> null;
+            };
+        }
+
+        @Override
+        protected ItemDisplayContext getTransformTypeForStack(GeoBone bone, ItemStack stack, AbstractSpellCastingMob animatable) {
+            return switch (bone.getName()) {
+                case LEFT_HAND, RIGHT_HAND -> ItemDisplayContext.THIRD_PERSON_RIGHT_HAND;
+                default -> ItemDisplayContext.NONE;
+            };
+        }
+
+        @Override
+        protected void renderStackForBone(PoseStack poseStack, GeoBone bone, ItemStack stack,
+                                          AbstractSpellCastingMob animatable, MultiBufferSource bufferSource,
+                                          float partialTick, int packedLight, int packedOverlay) {
+            if (RandomHumanEntityRenderer.this.shieldWasHidden &&
+                    !RandomHumanEntityRenderer.this.hiddenShield.isEmpty()) {
+                animatable.setItemSlot(net.minecraft.world.entity.EquipmentSlot.OFFHAND,
+                        RandomHumanEntityRenderer.this.hiddenShield);
+            }
+
+            if (stack == RandomHumanEntityRenderer.this.mainHandItem) {
+                poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
+
+                if (stack.getItem() instanceof ShieldItem) {
+                    poseStack.translate(0, 0.125, -0.25);
+                }
+            }
+            else if (stack == RandomHumanEntityRenderer.this.offhandItem) {
+                poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
+
+                if (stack.getItem() instanceof ShieldItem) {
+                    poseStack.translate(0, 0.125, 0.25);
+                    poseStack.mulPose(Axis.YP.rotationDegrees(180));
+                }
+            }
+
+            super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
+
+            RandomHumanEntityRenderer.this.hiddenShield = ItemStack.EMPTY;
+            RandomHumanEntityRenderer.this.shieldWasHidden = false;
+        }
+
+        private boolean shouldRenderCustom(ItemStack stack) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            return stack.getItem() instanceof ShieldItem;
+        }
     }
 }

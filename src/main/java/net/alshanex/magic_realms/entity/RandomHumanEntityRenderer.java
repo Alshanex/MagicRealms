90001daf5c00df843296a491be998029104aeffa
Alshanex <discordpermacc02@gmail.com>
2025-05-28 10:26:59 +0200

    Shield fixed but renders twice

--- src/main/java/net/alshanex/magic_realms/entity/RandomHumanEntityRenderer.java
+++ src/main/java/net/alshanex/magic_realms/entity/RandomHumanEntityRenderer.java
@@ -1,17 +1,88 @@
 package net.alshanex.magic_realms.entity;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShieldItem;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer;
 
 public class RandomHumanEntityRenderer extends AbstractSpellCastingMobRenderer {
+    private static final String LEFT_HAND = "bipedHandLeft";
+    private static final String RIGHT_HAND = "bipedHandRight";
+
+    protected ItemStack mainHandItem;
+    protected ItemStack offhandItem;
+
     public RandomHumanEntityRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new RandomHumanEntityModel());
+
+        addRenderLayer(new BlockAndItemGeoLayer<>(this) {
+            @Nullable
+            @Override
+            protected ItemStack getStackForBone(GeoBone bone, AbstractSpellCastingMob animatable) {
+                // Retrieve the items in the entity's hands for the relevant bone
+                return switch (bone.getName()) {
+                    case LEFT_HAND -> animatable.isLeftHanded() ?
+                            RandomHumanEntityRenderer.this.mainHandItem : RandomHumanEntityRenderer.this.offhandItem;
+                    case RIGHT_HAND -> animatable.isLeftHanded() ?
+                            RandomHumanEntityRenderer.this.offhandItem : RandomHumanEntityRenderer.this.mainHandItem;
+                    default -> null;
+                };
+            }
+
+            @Override
+            protected ItemDisplayContext getTransformTypeForStack(GeoBone bone, ItemStack stack, AbstractSpellCastingMob animatable) {
+                // Apply the camera transform for the given hand
+                return switch (bone.getName()) {
+                    case LEFT_HAND, RIGHT_HAND -> ItemDisplayContext.THIRD_PERSON_RIGHT_HAND;
+                    default -> ItemDisplayContext.NONE;
+                };
+            }
+
+            // Do some quick render modifications depending on what the item is
+            @Override
+            protected void renderStackForBone(PoseStack poseStack, GeoBone bone, ItemStack stack, AbstractSpellCastingMob animatable,
+                                              MultiBufferSource bufferSource, float partialTick, int packedLight, int packedOverlay) {
+                if (stack == RandomHumanEntityRenderer.this.mainHandItem) {
+                    poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
+
+                    if (stack.getItem() instanceof ShieldItem)
+                        poseStack.translate(0, 0.125, -0.25);
+                }
+                else if (stack == RandomHumanEntityRenderer.this.offhandItem) {
+                    poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
+
+                    if (stack.getItem() instanceof ShieldItem) {
+                        poseStack.translate(0, 0.125, 0.25);
+                        poseStack.mulPose(Axis.YP.rotationDegrees(180));
+                    }
+                }
+
+                super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
+            }
+        });
     }
 
     @Override
     public ResourceLocation getTextureLocation(AbstractSpellCastingMob animatable) {
         return ((RandomHumanEntity)animatable).getTextureConfig().getSkinTexture();
+    }
+
+    @Override
+    public void preRender(PoseStack poseStack, AbstractSpellCastingMob animatable, BakedGeoModel model, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour);
+
+        this.mainHandItem = animatable.getMainHandItem();
+        this.offhandItem = animatable.getOffhandItem();
     }
 }

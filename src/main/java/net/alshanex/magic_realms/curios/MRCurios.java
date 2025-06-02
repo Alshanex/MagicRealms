ff83055b3e46948602ae2ca2241a370f9a5b9d5c
Alshanex <discordpermacc02@gmail.com>
2025-06-02 16:35:45 +0200

    Added curio but not showing

---
+++ src/main/java/net/alshanex/magic_realms/curios/MRCurios.java
@@ -0,0 +1,5 @@
+package net.alshanex.magic_realms.curios;
+
+public class MRCurios {
+    public static String TABLET_SLOT = "tablet";
+}
--- src/main/java/net/alshanex/magic_realms/datagen/MRLanguageProvider.java
+++ src/main/java/net/alshanex/magic_realms/datagen/MRLanguageProvider.java
@@ -14,7 +14,7 @@ public class MRLanguageProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         // Items
-        add(MRItems.HUMAN_TEAM_ITEM.get(), "Hero Journal");
+        add(MRItems.HUMAN_TEAM_ITEM.get(), "Summoning Tablet");
         add(MRItems.HUMAN_INFO_ITEM.get(), "Human Information Scroll");
 
         // GUI translations
@@ -32,9 +32,13 @@ public class MRLanguageProvider extends LanguageProvider {
         add("gui.magic_realms.human_info.no_attributes", "No attributes");
         add("gui.magic_realms.human_info.entity_not_found", "Entity not found");
         add("gui.magic_realms.human_info.not_linked", "Not linked to entity");
-        add("gui.magic_realms.human_team.title", "Hero Journal");
+        add("gui.magic_realms.human_team.title", "Summoning Tablet");
 
         //Entities
         add("entity.magic_realms.warden_clone", "Sculk Guardian");
+
+        //Curios
+        add("curios.identifier.tablet", "Summoning Tablet");
+        add("curios.modifiers.tablet", "When equipped as Tablet:");
     }
 }

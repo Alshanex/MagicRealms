929f3d1375a38c0f5a25411fa78379b4f968cac3
Alshanex <discordpermacc02@gmail.com>
2025-08-26 11:27:20 +0200

    Revert "Very basic version of the chatting system"
    This reverts commit f3f0559dc9c3e5b5a3013d4b891f580d7b0cbcf0.

--- src/main/java/net/alshanex/magic_realms/util/chat_system/InputAnalysis.java
+++
@@ -1,9 +0,0 @@
-package net.alshanex.magic_realms.util.chat_system;
-
-public class InputAnalysis {
-    ConversationIntent intent = ConversationIntent.UNKNOWN;
-    double sentiment = 0;
-    String topic = "";
-    boolean isCorrection = false;
-    String correctedTopic = "";
-}
--- src/main/java/net/alshanex/magic_realms/util/chat_system/SpeechPattern.java
+++
@@ -1,5 +0,0 @@
-package net.alshanex.magic_realms.util.chat_system;
-
-public enum SpeechPattern {
-    NORMAL, FORMAL, CASUAL, POETIC, BRIEF, VERBOSE
-}

5f211904c101944e6e0a150c6f79fac7a3690307
Alshanex <discordpermacc02@gmail.com>
2025-09-06 19:09:03 +0200

    Added inventory to entities

--- src/main/java/net/alshanex/magic_realms/entity/RandomHumanEntity.java
+++ src/main/java/net/alshanex/magic_realms/entity/RandomHumanEntity.java
@@ -43,6 +43,7 @@ import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -53,18 +54,17 @@ import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RangedBowAttackGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.RangedAttackMob;
+import net.minecraft.world.entity.npc.InventoryCarrier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib.animation.*;
 import software.bernie.geckolib.animation.AnimationState;
 
@@ -73,7 +73,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacker, RangedAttackMob {
+public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacker, RangedAttackMob, InventoryCarrier {
     private static final EntityDataAccessor<Integer> GENDER = SynchedEntityData.defineId(RandomHumanEntity.class, EntityDataSerializers.INT);
     private static final EntityDataAccessor<Integer> ENTITY_CLASS = SynchedEntityData.defineId(RandomHumanEntity.class, EntityDataSerializers.INT);
     private static final EntityDataAccessor<Boolean> INITIALIZED = SynchedEntityData.defineId(RandomHumanEntity.class, EntityDataSerializers.BOOLEAN);
@@ -88,6 +88,15 @@ public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacke
     private boolean appearanceGenerated = false;
 
     private List<SchoolType> magicSchools = new ArrayList<>();
+
+    private AbstractSpell lastCastingSpell = null;
+    private boolean wasCasting = false;
+    private boolean goalsInitialized = false;
+    private boolean wasChargingArrow = false;
+    private boolean shouldPlayChargeAnimation = false;
+
+    private static final int INVENTORY_SIZE = 27;
+    private final SimpleContainer inventory = new SimpleContainer(INVENTORY_SIZE);
 
     public RandomHumanEntity(EntityType<? extends AbstractSpellCastingMob> entityType, Level level) {
         super(entityType, level);
@@ -360,11 +369,37 @@ public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacke
                 .add(Attributes.MOVEMENT_SPEED, .25);
     }
 
-    private AbstractSpell lastCastingSpell = null;
-    private boolean wasCasting = false;
-    private boolean goalsInitialized = false;
-    private boolean wasChargingArrow = false;
-    private boolean shouldPlayChargeAnimation = false;
+    @Override
+    public SimpleContainer getInventory() {
+        return inventory;
+    }
+
+    @Override
+    public boolean canPickUpLoot() {
+        return true;
+    }
+
+    @Override
+    protected void pickUpItem(ItemEntity itemEntity) {
+        InventoryCarrier.pickUpItem(this, this, itemEntity);
+    }
+
+    @Override
+    public boolean wantsToPickUp(ItemStack stack) {
+        return !stack.isEmpty() && this.getInventory().canAddItem(stack);
+    }
+
+    public boolean hasItem(ItemStack itemStack, int count) {
+        return this.getInventory().countItem(itemStack.getItem()) >= count;
+    }
+
+    public int getItemCount(ItemStack itemStack) {
+        return this.getInventory().countItem(itemStack.getItem());
+    }
+
+    public void clearInventory() {
+        this.getInventory().clearContent();
+    }
 
     @Override
     public void tick() {
@@ -562,7 +597,7 @@ public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacke
 
         if (!entityName.isEmpty()) {
 
-            Component nameComponent = Component.literal(entityName + "   Lv. " + data.getCurrentLevel());
+            Component nameComponent = Component.literal(entityName + " Lv. " + data.getCurrentLevel());
             this.setCustomName(nameComponent);
             this.setCustomNameVisible(true);
         }
@@ -669,6 +704,8 @@ public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacke
         }
 
         OwnerHelper.serializeOwner(compound, summonerUUID);
+
+        this.writeInventoryToTag(compound, this.registryAccess());
     }
 
     @Override
@@ -748,6 +785,8 @@ public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacke
         if(compound.getBoolean("Standby")){
             clearMovementGoals();
         }
+
+        this.readInventoryFromTag(compound, this.registryAccess());
     }
 
     private boolean isAlliedHelper(Entity entity) {
@@ -767,6 +806,17 @@ public class RandomHumanEntity extends NeutralWizard implements IAnimatedAttacke
 
     @Override
     public void die(net.minecraft.world.damagesource.DamageSource damageSource) {
+        if (!level().isClientSide) {
+            for (int i = 0; i < inventory.getContainerSize(); i++) {
+                ItemStack itemStack = inventory.getItem(i);
+                if (!itemStack.isEmpty()) {
+                    ItemEntity itemEntity = new ItemEntity(level(),
+                            getX(), getY() + 0.5, getZ(), itemStack);
+                    itemEntity.setDefaultPickUpDelay();
+                    level().addFreshEntity(itemEntity);
+                }
+            }
+        }
         super.die(damageSource);
     }
 

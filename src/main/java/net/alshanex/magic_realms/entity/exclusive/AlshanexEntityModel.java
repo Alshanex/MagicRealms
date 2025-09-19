55a7082821ca74974e2200d95e03969b9f699ac7
Alshanex <discordpermacc02@gmail.com>
2025-09-19 16:26:36 +0200

    Fixed model animations

--- src/main/java/net/alshanex/magic_realms/entity/exclusive/AlshanexEntityModel.java
+++ src/main/java/net/alshanex/magic_realms/entity/exclusive/AlshanexEntityModel.java
@@ -3,12 +3,13 @@ package net.alshanex.magic_realms.entity.exclusive;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobModel;
 import net.alshanex.magic_realms.MagicRealms;
+import net.alshanex.magic_realms.entity.AbstractMercenaryEntityModel;
 import net.alshanex.magic_realms.entity.random.RandomHumanEntity;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.cache.object.GeoBone;
 
-public class AlshanexEntityModel extends AbstractSpellCastingMobModel {
+public class AlshanexEntityModel extends AbstractMercenaryEntityModel {
     @Override
     public ResourceLocation getTextureResource(AbstractSpellCastingMob entity) {
         return ResourceLocation.fromNamespaceAndPath(MagicRealms.MODID, "textures/entity/exclusive_mercenaries/alshanex.png");
--- src/main/java/net/alshanex/magic_realms/entity/random/RandomHumanEntityModel.java
+++ src/main/java/net/alshanex/magic_realms/entity/random/RandomHumanEntityModel.java
@@ -2,11 +2,12 @@ package net.alshanex.magic_realms.entity.random;
 
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobModel;
+import net.alshanex.magic_realms.entity.AbstractMercenaryEntityModel;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.cache.object.GeoBone;
 
-public class RandomHumanEntityModel extends AbstractSpellCastingMobModel {
+public class RandomHumanEntityModel extends AbstractMercenaryEntityModel {
     @Override
     public ResourceLocation getTextureResource(AbstractSpellCastingMob entity) {
         return ResourceLocation.fromNamespaceAndPath("minecraft", "textures/entity/player/wide/steve.png");

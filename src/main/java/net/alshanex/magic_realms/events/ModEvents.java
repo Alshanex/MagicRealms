1b6e3e600d47fb2fae9d92f8bc2b85b4c3979704
Alshanex <discordpermacc02@gmail.com>
2025-09-27 20:55:52 +0200

    Added spawning, missing checking how slime death is handled and friendly spell

---
+++ src/main/java/net/alshanex/magic_realms/events/ModEvents.java
@@ -0,0 +1,36 @@
+package net.alshanex.magic_realms.events;
+
+import net.alshanex.magic_realms.MagicRealms;
+import net.alshanex.magic_realms.registry.MREntityRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
+
+@EventBusSubscriber(modid = MagicRealms.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class ModEvents {
+    @SubscribeEvent
+    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event){
+        event.register(MREntityRegistry.MAGIC_SLIME.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                (entityType, level, spawnType, pos, random) -> checkDaytimeSpawn(entityType, level, spawnType, pos, random, Mob::checkMobSpawnRules), RegisterSpawnPlacementsEvent.Operation.REPLACE);
+    }
+
+    private static <T extends Mob> boolean checkDaytimeSpawn(EntityType<T> entityType, ServerLevelAccessor level,
+                                                         net.minecraft.world.entity.MobSpawnType spawnType,
+                                                         BlockPos pos, RandomSource random,
+                                                         SpawnPlacements.SpawnPredicate<T> originalSpawnRule) {
+
+        long dayTime = level.getLevelData().getDayTime() % 24000;
+        boolean isDaytime = dayTime >= 0 && dayTime < 12000;
+
+        if(!isDaytime){
+            return false;
+        }
+
+        return originalSpawnRule.test(entityType, level, spawnType, pos, random);
+    }
+}

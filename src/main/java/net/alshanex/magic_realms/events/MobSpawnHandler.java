c13b2e31c1ef60b77601b397bd60beaccbc2ae19
Alshanex <discordpermacc02@gmail.com>
2026-05-01 16:42:32 +0200

    Added bandits to pillager outposts

--- src/main/java/net/alshanex/magic_realms/events/MobSpawnHandler.java
+++ src/main/java/net/alshanex/magic_realms/events/MobSpawnHandler.java
@@ -1,18 +1,14 @@
 package net.alshanex.magic_realms.events;
 
 import net.alshanex.magic_realms.MagicRealms;
-import net.alshanex.magic_realms.entity.creeper.MagicCreeperEntity;
+import net.alshanex.magic_realms.entity.random.hostile.HostileRandomHumanEntity;
 import net.alshanex.magic_realms.entity.tim.TimEntity;
 import net.alshanex.magic_realms.registry.MREntityRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.animal.horse.ZombieHorse;
-import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.entity.monster.Zombie;
-import net.minecraft.world.entity.monster.ZombieVillager;
-import net.minecraft.world.entity.monster.ZombifiedPiglin;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -49,6 +45,26 @@ public class MobSpawnHandler {
                 // Cancel the original zombie spawn
                 event.setCanceled(true);
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPillagerSpawn(EntityJoinLevelEvent event) {
+        if (event.getLevel().isClientSide()) return;
+        if (!(event.getLevel() instanceof ServerLevel serverLevel)) return;
+        if (!(event.getEntity() instanceof Pillager pillager)) return;
+
+        BlockPos pos = pillager.blockPosition();
+
+        // 10% roll
+        if (pillager.getRandom().nextFloat() >= 0.10f) return;
+
+        HostileRandomHumanEntity bandit = new HostileRandomHumanEntity(event.getLevel(), "magic_realms:normal_bandit");
+        bandit.moveTo(pillager.getX(), pillager.getY(), pillager.getZ(), pillager.getYRot(), pillager.getXRot());
+        bandit.finalizeSpawn(serverLevel, serverLevel.getCurrentDifficultyAt(pos), MobSpawnType.NATURAL, null);
+
+        if (event.getLevel().addFreshEntity(bandit)) {
+            event.setCanceled(true);
         }
     }
 }
